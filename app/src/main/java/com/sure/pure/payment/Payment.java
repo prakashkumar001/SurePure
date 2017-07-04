cdcd28b0558dd205928d0046ac4abdbf136bfc5e
Prakash <akshav00@gmail.com>
2017-07-04 10:25:50 +0530

    Payment

--- app/src/main/java/com/sure/pure/payment/Payment.java
+++ app/src/main/java/com/sure/pure/payment/Payment.java
@@ -1,9 +1,11 @@
 package com.sure.pure.payment;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.sure.pure.R;
 
@@ -13,6 +15,7 @@ import com.sure.pure.R;
 
 public class Payment extends AppCompatActivity {
     WebView webView;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +25,74 @@ public class Payment extends AppCompatActivity {
         webView.getSettings().setSupportZoom(true);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(true);
-        webView.loadUrl("http://google.com");
-        webView.setHorizontalScrollBarEnabled(false);
+        startWebView("http://www.androidexample.com/media/webview/login.html");
+    }
+
+    private void startWebView(String url) {
+        progressDialog = new ProgressDialog(Payment.this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+        //Create new webview Client to show progress dialog
+        //When opening a url or click on link
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            //If you will not use this method url links are opeen in new brower not in webview
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            //Show loader on url load
+            public void onLoadResource (WebView view, String url) {
+
+            }
+            public void onPageFinished(WebView view, String url) {
+                try{
+                    if (progressDialog.isShowing()) {
+                        progressDialog.dismiss();
+                        progressDialog = null;
+                    }
+                }catch(Exception exception){
+                    exception.printStackTrace();
+                }
+            }
+
+        });
+
+        // Javascript inabled on webview
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // Other webview options
+        /*
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(false);
+        webView.getSettings().setBuiltInZoomControls(true);
+        */
+
+        /*
+         String summary = "<html><body>You scored <b>192</b> points.</body></html>";
+         webview.loadData(summary, "text/html", null);
+         */
+
+        //Load url in webview
+        webView.loadUrl(url);
+
+
+    }
+
+    // Open previous opened link from history on webview when back button pressed
+
+    @Override
+    // Detect when the back button is pressed
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            // Let the system handle the back button
+            super.onBackPressed();
+        }
     }
 }
